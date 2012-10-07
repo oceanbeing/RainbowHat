4011abf1a5f6b72b11998e6826fce97e5008a53a
Pat Oceana <oceanbeing@msn.com>
2012-10-06 19:15:28 -0700

    Homwork Baselin

---
+++ Mickey.Phoenix/Homework/Homework 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I only deserve a B+ for such a boring assignment -- my students deserve good extra credit work to do, too.");
+            Console.ReadKey();
+        }
+    }
+}
